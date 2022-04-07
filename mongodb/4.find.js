54947857eb8d8e5468bac4a932cf12a907185192
Selva <selvamanan369@gmail.com>
2022-04-07 23:16:56 +0530

    update

--- mongodb/4.find.js
+++ mongodb/4.find.js
@@ -4,12 +4,27 @@ var url="mongodb://localhost:27017/";
 MongoClient.connect(url,function(err,db){
     if (err) throw err;
     var dbname=db.db("first");
-   dbname.collection("student").find({}).toArray(function(err,res){
+   /*dbname.collection("student").find({}).toArray(function(err,res){
         if (err) throw err;
         console.log(res);
 
         db.close();
     });
-        
+        */
+/*
+    dbname.collection("student").find({}, { projection: { _id: 0, name: 1, age: 1 } }).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+      });
+
+*/
+      const query = {first_name:/^K/};
+
+      // Find by query & regular expression
+      dbname.collection("student").find(query).toArray((err,res)=>{
+          if(err) throw err;
+          console.log(res);
+      });
 
 });
